6ec64fc7cc0b7d30469e3d056706c8fc5dbbfe05
Dmitry <dima.gotovko.sport@mail.ru>
2022-03-23 21:51:08 +0300

    CommandsFromDmitrijGotovko

--- src/main/kotlin/commands/Timer.kt
+++ src/main/kotlin/commands/Timer.kt
@@ -10,22 +10,27 @@ class TimerCommand : Command {
     override fun execute(text: String) {
         try {
             val inputValue = text.toInt()
-            if (inputValue in 5..10) {
-                for (i in inputValue downTo 1) {
+            if (inputValue in MIN_VALUE_INPUT..MAX_VALUE_INPUT) {
+                for (i in inputValue downTo MIN_VALUE_TAIMER) {
                     println("$i..")
-                    Thread.sleep(1000)
+                    Thread.sleep(MILLIS.toLong())
                 }
             } else {
-                for (i in 5 downTo 0) {
+                for (i in MIN_VALUE_INPUT downTo MIN_VALUE_TAIMER) {
                     println("$i..")
-                    Thread.sleep(1000)
+                    Thread.sleep(MILLIS.toLong())
                 }
             }
             println(0)
-        }catch (e : NumberFormatException) {
+        } catch (e: NumberFormatException) {
             println("Некорректный ввод")
+        }
     }
-}
 
-
+    companion object {
+        private const val MILLIS = 1000
+        private const val MAX_VALUE_INPUT = 10
+        private const val MIN_VALUE_INPUT = 5
+        private const val MIN_VALUE_TAIMER = 1
+    }
 }
