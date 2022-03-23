d9029a5a1cc26cc2b84e495c2e613d9fb5430ff3
Dmitry <dima.gotovko.sport@mail.ru>
2022-03-23 07:08:54 +0300

    CommandsFromDmitrijGotovko

--- src/main/kotlin/commands/HelloFromDmitry.kt
+++ src/main/kotlin/commands/HelloFromDmitry.kt
@@ -4,7 +4,7 @@ import Command
 
 class Dmitry : Command {
     override val keyword = "Gotovko"
-    override val description = "Greetings from dmitry"
+    override val description = "Greetings from Dmitry"
 
     override fun execute(text: String) {
         println("Hello from Dmitry")
